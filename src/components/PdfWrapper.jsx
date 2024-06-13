105a03e9d791209dec0e7f3a26924d281044b22f
nuvideveloper <jgfchona@gmail.com>
2024-06-13 15:05:07 -0500

    Formulario para escribir la resolución que va en el PDF

--- src/components/PdfWrapper.jsx
+++ src/components/PdfWrapper.jsx
@@ -1,35 +1,35 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { useSubject } from './../context/ScheduleContext';
-import { getSemesters, getSubjectsFromAPI } from "../api/api";
+import { getSemesters } from "../api/api";
+import { useLocation } from 'react-router-dom';
 
 import './../styles/pdf_wrapper.css';
 import Logo from './../assets/logo.jpg';
-import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 const PdfWrapper = () => {
-  const {firstWeek, secondWeek} = useSubject();
-  const [availableSubjectsBySemester, setAvailableSubjectsBySemester] =
-    useState([]);
-  const reportRef = useRef(null)
+  const { firstWeek, secondWeek } = useSubject();
+  const [availableSubjectsBySemester, setAvailableSubjectsBySemester] = useState([]);
+  const reportRef = useRef(null);
+  const location = useLocation();
+  const { resolucion, fecha, descripcion, director, articulo } = location.state || {};
 
   useEffect(() => {
     const fetchAvailableSubjects = async () => {
       try {
-        const subjects = [...firstWeek, ...secondWeek]
+        const subjects = [...firstWeek, ...secondWeek];
         const semesters = await getSemesters();
         let subjectsBySemester = [];
         Object.entries(semesters.semestres).forEach(([semester, _]) => {
           const curr = {};
           curr.id = semester;
-
           curr.subjects = subjects.filter((subject) =>
             _.some((el) => el.id === subject.id)
           ).filter(subject => Array.from(subject.schedules.values()).length > 0)
           .map(subject => Array.from(subject.schedules.values()))
-          .reduce((acc, el) => [...acc, ...el], [])
+          .reduce((acc, el) => [...acc, ...el], []);
 
-          if (curr.subjects.length > 0) 
+          if (curr.subjects.length > 0)
             subjectsBySemester.push(curr);
         });
 
@@ -43,112 +43,72 @@ const PdfWrapper = () => {
       }
     };
     fetchAvailableSubjects();
-  }, [])
+  }, [firstWeek, secondWeek]);
 
   const printDocument = () => {
-    //generatePdf(null);
-      const doc = new jsPDF({ format: [1440, 2160] });
-
-      doc.html(reportRef.current, {
-        callback: function (pdf) {
-          pdf.save('schedule.pdf');
-        },
-      });
+    const doc = new jsPDF({ format: [1440, 2160] });
+    doc.html(reportRef.current, {
+      callback: function (pdf) {
+        pdf.save('schedule.pdf');
+      },
+    });
   };
 
   const renderTable = () => {
     return (
-      <div class="center-div" id="tablasContainer">
+      <div className="center-div" id="tablasContainer">
         {availableSubjectsBySemester.length > 0 && availableSubjectsBySemester.map(el => {
           return (
-            <table class="table table-striped">
+            <table className="table table-striped" key={el.id}>
               <thead>
                 <tr>
-                  <th colspan="4" class="centered">
+                  <th colSpan="4" className="centered">
                     SEMESTRE {el.id}
                   </th>
                 </tr>
               </thead>
               <tbody>
                 {el.subjects.map((schedule) => (
-                    <tr>
-                      <td className="letter-spacingX">
-                        {schedule.materia_id}-{schedule.grupo_id}
-                      </td>
-                      <td>{schedule.name}</td>
-                      <td className="letter-spacing">{schedule.formatDay.toISOString().split('T')[0].replace(/-/g, '/')}</td>
-                      <td className="letter-spacing">{schedule.hora_inicio} - {schedule.hora_fin} {schedule.salon}</td>
-                    </tr>
-                  ))
-                }
-              </tbody>
-            </table>
-          )
-        })}
-        {/* {secondWeek.length > 0 && (
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th colspan="4" class="centered">
-                  SEMANA 2
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {secondWeek.map((subject) =>
-                Array.from(subject.schedules.values()).map((schedule) => (
-                  <tr>
+                  <tr key={schedule.materia_id + '-' + schedule.grupo_id}>
                     <td className="letter-spacingX">
-                      {subject.id}-{schedule.grupo_id}
+                      {schedule.materia_id}-{schedule.grupo_id}
                     </td>
-                    <td>{subject.name}</td>
+                    <td>{schedule.name}</td>
                     <td className="letter-spacing">{schedule.formatDay.toISOString().split('T')[0].replace(/-/g, '/')}</td>
                     <td className="letter-spacing">{schedule.hora_inicio} - {schedule.hora_fin} {schedule.salon}</td>
                   </tr>
-                ))
-              )}
-            </tbody>
-          </table>
-        )} */}
+                ))}
+              </tbody>
+            </table>
+          );
+        })}
       </div>
     );
   };
 
   return (
-    <div class="pdf-size" ref={reportRef}>
-      <div class="top">
+    <div className="pdf-size" ref={reportRef}>
+      <div className="top">
         <img src={Logo} alt="Descripción de la imagen" />
       </div>
-      <div class="text-size">
-        <h3>RESOLUCIÓN N° 03 </h3>
-        <h3>31 de octubre de 2023</h3>
-        <br />
-        <h4>
-          POR LA CUAL SE PROGRAMAN LAS FECHAS DE EXÁMENES FINALES DEL PLAN DE
-          ESTUDIOS DE INGENIERÍA DE SISTEMAS, CORRESPONDIENTES  AL SEMESTRE 2023
-          - 2.
-        </h4>
-        <p>
-          EL DIRECTOR DEL PLAN DE ESTUDIOS DE INGENIERÍA DE SISTEMAS DE LA UFPS,
-          en uso de sus facultades reglamentadas,
-        </p>
+      <div className="text-size">
+        <br /><br /><br /><br /><br /><br />
+        <h3>RESOLUCIÓN N° {resolucion}</h3>
+        <h3>{fecha}</h3>
+        <h4>{descripcion}</h4>
+        <p>{director}</p>
         <br />
         <h3>RESUELVE:</h3>
-        <h4>
-          ARTÍCULO ÚNICO: Establecer las fechas de EXÁMENES FINALES <br />{' '}
-          correspondientes a los estudiantes del Plan de Estudios de INGENIERÍA
-          DE SISTEMAS
-        </h4>
-        <br />
-        <div class="center-div" id="tablasContainer">
+        <h4>{articulo}</h4>
+        <div className="center-div" id="tablasContainer">
           {renderTable()}
         </div>
         <br />
-        <button class="butn" id="myguionpdf" onClick={printDocument}>
+        <button className="butn" id="myguionpdf" onClick={printDocument}>
           Descargar PDF
         </button>
       </div>
-      <br></br>
+      <br />
     </div>
   );
 };
--- src/views/ViewSchedule.tsx
+++ src/views/ViewSchedule.tsx
@@ -1,20 +1,22 @@
-import { FC, forwardRef, useRef, useState, useEffect } from 'react';
+import { FC, forwardRef, useRef, useState, useEffect } from "react";
 
-import './../style.css';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment from 'moment';
-import { useSubject } from '../context/ScheduleContext';
-import CalendarWeekView from '../components/CalendarWeekView';
-import { useNavigate } from 'react-router-dom';
+import "./../style.css";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import { useSubject } from "../context/ScheduleContext";
+import CalendarWeekView from "../components/CalendarWeekView";
+import { useNavigate } from "react-router-dom";
 //import { generatePdf } from '../api/api';
-import PdfWrapper from './../components/PdfWrapper';
-import { getDates } from './../backend';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import PdfWrapper from "./../components/PdfWrapper";
+import { getDates } from "./../backend";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
-
-const AvailableSubjectsWeekCalendar: React.FC<{ subjects: any[], initialDate: any }> = ({  subjects, initialDate }) => {
+const AvailableSubjectsWeekCalendar: React.FC<{
+  subjects: any[];
+  initialDate: any;
+}> = ({ subjects, initialDate }) => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -24,9 +26,9 @@ const AvailableSubjectsWeekCalendar: React.FC<{ subjects: any[], initialDate: an
         newEvents.push({
           title: subject.name,
           day: schedule.dia,
-          startHour: parseInt(schedule.hora_inicio.split(':')[0], 10),
-          endHour: parseInt(schedule.hora_fin.split(':')[0], 10),
-          description: 'Grupo ' + schedule.grupo_id,
+          startHour: parseInt(schedule.hora_inicio.split(":")[0], 10),
+          endHour: parseInt(schedule.hora_fin.split(":")[0], 10),
+          description: "Grupo " + schedule.grupo_id,
         });
       });
     });
@@ -39,8 +41,16 @@ const AvailableSubjectsWeekCalendar: React.FC<{ subjects: any[], initialDate: an
 
 const ViewSchedule: FC<{ name: string }> = ({ name }) => {
   const calendarRef = useRef();
-  const { firstWeek, secondWeek, initialDate, secondInitialDate } = useSubject();
-
+  const { firstWeek, secondWeek, initialDate, secondInitialDate } =
+    useSubject();
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState({
+    resolucion: "",
+    fecha: "",
+    descripcion: "",
+    director: "",
+    articulo: "",
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -50,28 +60,138 @@ const ViewSchedule: FC<{ name: string }> = ({ name }) => {
     };
   }, []);
 
-  const goToAnotherPage = () => {
-    navigate('/home/report');
+  useEffect(() => {
+    if (showForm) {
+      // Obtenemos la posición del formulario
+      const formPosition = document.querySelector('.container').getBoundingClientRect().top + window.scrollY;
+      // Desplazamos la página hacia la posición del formulario
+      window.scrollTo({
+        top: formPosition,
+        behavior: 'smooth' // Para que el desplazamiento sea suave
+      });
+    }
+  }, [showForm]);
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    navigate("/home/report", { state: formData });
+  };
+
+  const showPdfForm = () => {
+    setShowForm(true);
   };
 
   return (
     <>
       <div ref={calendarRef}>
-        <h2> Horarios establecidad para la primera semana </h2>
+        <h2> Horarios establecidos para la primera semana </h2>
         <div className="calendar">
-        <AvailableSubjectsWeekCalendar subjects={firstWeek} initialDate={initialDate}/>
+          <AvailableSubjectsWeekCalendar
+            subjects={firstWeek}
+            initialDate={initialDate}
+          />
         </div>
-        <h2> Horarios establecidad para la segunda semana </h2>
+        <h2> Horarios establecidos para la segunda semana </h2>
         <div className="calendar">
-        <AvailableSubjectsWeekCalendar subjects={secondWeek} initialDate={secondInitialDate}/>
+          <AvailableSubjectsWeekCalendar
+            subjects={secondWeek}
+            initialDate={secondInitialDate}
+          />
         </div>
       </div>
       <div className="button-wrapper">
-      <button className="next-page" onClick={goToAnotherPage}>
-        Visualizar PDF
-      </button>
+        <button className="next-page" onClick={showPdfForm}>
+          Visualizar PDF
+        </button>
       </div>
-      <br></br>
+      {showForm && (
+        <div className="container">
+          <div className="form-wrapper">
+            <form onSubmit={handleFormSubmit}>
+              <h2>EJEMPLO: </h2>
+              <h3>RESOLUCIÓN N° 03 </h3>
+              <h3>19 de marzo de 2024</h3>
+              <br />
+              <h4>
+                POR LA CUAL SE PROGRAMAN LAS FECHAS DE EXAMENES FINALES DEL PLAN
+                DE ESTUDIOS DE INGENIERÍA DE SISTEMAS, CORRESPONDIENTES AL SEMESTRE
+                2024 - 1
+              </h4>
+              <p>
+                EL DIRECTOR DEL PLAN DE ESTUDIOS DE INGENIERÍA DE SISTEMAS DE LA
+                UFPS, en uso de sus facultades reglamentadas,
+              </p>
+              <br />
+              <h3>RESUELVE:</h3>
+              <h4>
+                ARTÍCULO ÚNICO: Establecer las fechas de EXÁMENES FINALES <br />{" "}
+                correspondientes a los estudiantes del Plan de Estudios de
+                INGENIERÍA DE SISTEMAS
+              </h4>
+              <br />
+              <label>
+                Resolución N°:
+                <input
+                  type="text"
+                  name="resolucion"
+                  value={formData.resolucion}
+                  onChange={handleFormChange}
+                  required
+                />
+              </label>
+              <br />
+              <label>
+                Fecha:
+                <input
+                  type="date"
+                  name="fecha"
+                  value={formData.fecha}
+                  onChange={handleFormChange}
+                  required
+                />
+              </label>
+              <br />
+              <label>
+                Descripción:
+                <textarea
+                  name="descripcion"
+                  value={formData.descripcion}
+                  onChange={handleFormChange}
+                  required
+                />
+              </label>
+              <br />
+              <label>
+                Director:
+                <input
+                  type="text"
+                  name="director"
+                  value={formData.director}
+                  onChange={handleFormChange}
+                  required
+                />
+              </label>
+              <label>
+                Artículo Unico:
+                <textarea
+                  name="articulo"
+                  value={formData.articulo}
+                  onChange={handleFormChange}
+                  required
+                />
+              </label>
+              <br />
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+        </div>
+      )}
+      <br />
     </>
   );
 };
